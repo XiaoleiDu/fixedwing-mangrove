fbb2537613517e697ea4b876caf0a1fb1c51796a
XiaoleiDu <59750876+XiaoleiDu@users.noreply.github.com>
2022-05-20 17:57:21 -0700

    Update of Camera control file and new read usb file
    The camera control file will now have indexed pictures written to external usb drive address

--- control_camera.py
+++ control_camera.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import cv2
-
-
+import usb_trial
+import os
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -12,13 +12,16 @@ cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # set new dimensionns to cam object (not cap)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-
+i = 1
 while True: 
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
+        directory = str((usb_trial.get_mount_points()[0][1]))
+        os.chdir(directory)
         ret,frame = cap.read() # return a single frame in variable `frame`
-        cv2.imwrite('1.png',frame)
+        cv2.imwrite(str(i)+'.png',frame)
         time.sleep(1)
+        i+=1
 
 
 
